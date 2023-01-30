a51d9951efadf2fba49ecb88115f987c5fd698df
Jan Gerstenberg <jangerstenberg2001@gmail.com>
2023-01-30 11:38:12 +0100

    Code clean up
    Adjusted some things. Cleaned up dead code

--- nav.js
+++ nav.js
@@ -1,20 +1,42 @@
-const sections = document.querySelectorAll('section');
-const navLi = document.querySelectorAll('nav .container ul li');
+/*let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('body header nav .container ul li a i span');
 
-window.addEventListener('scroll', ()=>{
-    let current = 's1';
-    console.log(pageYOffset)
-    sections.forEach( section => {
-        const sectionTop = section.offsetTop;
-        const sectionHight = section.clientHeight;
-        if(pageYOffset < sectionTop){
-            current = section.getAttribute('id');
-        }
-    })
-    navLi.forEach( li => {
-        li.classList.remove('active');
-        if(li.classList.contains(current)){
-            li.classList.add('active');
-        }
-    })
-}) 
+window.onscroll = () => {
+
+    section.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+            
+            if (top >= offset && top < offset + height) {
+               navlinks.forEach(links => {
+                links.classList.add('active');
+                document.querySelector('nav .container ul li a i span[href*=' + id + ']').classList.add
+                ('active');
+
+            }); 
+            };
+
+            
+
+    });
+};
+
+*/
+
+const section = document.querySelectorAll('section');
+const link = document.querySelectorAll('nav-mitem');
+
+section.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    if(top >= offset && top < offset + height){
+        link.forEach(link => {
+            link.classList.remove('active');
+        })
+
+    }
+})
+
