2f267da1b4db3244e7be16a7310b0a40a822811b
Jan Gerstenberg <jangerstenberg2001@gmail.com>
2023-02-08 11:02:36 +0100

    Nav-link highlight
    Not yet completly functional. Threshold will be added later

--- nav.js
+++ nav.js
@@ -50,7 +50,7 @@ window.onscroll = function() {
 
 }
 */
-
+/*
 let s0 = document.querySelectorAll('#jang');
 let s1 = document.querySelectorAll('#projects');
 let s2 = document.querySelectorAll('#plannedprojects');
@@ -124,3 +124,27 @@ window.addEventListener('scroll', () => {
   makeNavLinksSmooth( );
   spyScrolling( );
   */
+
+  (function() {
+    'use strict';
+  
+    var section = document.querySelectorAll(".section");
+    var sections = {};
+    var i = 0;
+  
+    Array.prototype.forEach.call(section, function(e) {
+      sections[e.id] = e.offsetTop;
+    });
+  
+    window.onscroll = function() {
+      var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+  
+      for (i in sections) {
+        if (sections[i] <= scrollPosition) {
+          document.querySelector('.active').setAttribute('class', ' ');
+          document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+        }
+
+      }
+    };
+  })();
