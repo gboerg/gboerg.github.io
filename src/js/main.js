fd527ef42d01940dd6f2369d1e6f6eb3c898ec32
Jan Gerstenberg <jangerstenberg2001@gmail.com>
2023-02-10 13:34:49 +0100

    Does Vine Work?
    Hope it does

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import './../scss/main.scss';
+import './../js/nav.js';
+import './../js/darkmode.js';
+
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
-import {defineconfig} from 'vite'
+import {defineConfig} from 'vite'
+
+export default defineConfig ({
+    root: "src",
+    assetsInclude: [".assets/**/*.*"],
+    build: {
+        emptyOutDir: true,
+        outDir:"../dist"
+
+    }
+})
