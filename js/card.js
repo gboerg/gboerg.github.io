d1536c98e990d095c6aee75cac7b3828a36788a1
Jan Gerstenberg <jangerstenberg2001@gmail.com>
2023-02-16 11:23:41 +0100

    css card test

--- js/card.js
+++ js/card.js
@@ -1,10 +1,20 @@
-import './../scss/styles.scss';
+import sheet from './../scss/styles.css' assert { type: 'css' };
+document.adoptedStyleSheets = [sheet];
 
 const wrapper = document.querySelector('.cards');
+const cards = document.querySelectorAll('.card');
 
 wrapper.addEventListener('mousemove', function ($event) {
-    console.log($event.clientX);
-    console.log($event.clientY);
+    cards.forEach((card) => {
+        const rect = card.getBoundingClientRect();
+        const x = $event.clientX - rect.left
+        const y = $event.clientY - rect.top
+
+
+
+        card.style.setProperty('--xPos', `${x} px`);
+        card.style.setProperty('--yPos', `${y} px`);
+    });
     
 });
 
